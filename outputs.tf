bfd6a56ff5350fc40aa286f1123165043aedaa59
mygitcode1 <36421857+mygitcode1@users.noreply.github.com>
2022-05-24 23:09:51 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,26 @@
+output "vpc_name" {
+  value = var.vpc_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.default.id
+}
+
+output "public_subnet_list" {
+  value = aws_subnet.public-subnets.*.id
+}
+
+output "private_subnet_list" {
+  value = aws_subnet.private-subnets.*.id
+}
+output "public_subnet_lis_1" {
+  value = aws_subnet.public-subnets.0.id
+}
+
+output "environment" {
+  value = var.environment
+}
+
+output "igw" {
+  value = aws_internet_gateway.default-igw.id
+}
