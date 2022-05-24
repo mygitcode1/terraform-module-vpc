7c8b088e6a63682498a14d99c5e8b4702aae3f45
mygitcode1 <36421857+mygitcode1@users.noreply.github.com>
2022-05-24 23:10:21 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# VPC
+variable "vpc_cidr_block" {}
+variable "vpc_name" {}
+
+# Subnets
+variable "public_subnet_cidr" {}
+variable "azs" {}
+variable "region" {}
+variable "private_subnet_cidr" {}
+# variable "" {}
+# variable "" {}
+# variable "" {}
+# variable "" {}
+# variable "" {}
+# variable "" {}
